d074a192457e650eaf955f15b595c40ff3e90b70
Hamidou Diallo <dio.hamidou@gmail.com>
2019-08-23 05:25:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 modules.export = {
 
-  happyHolidays: happyHolidays = () =>{
+  happyHolidays: function(){
     return "Happy holidays!"
   },
   
-  happyHolidaysTo: happyHolidaysTo = name =>{
+  happyHolidaysTo: function(name){
     return `Happy holidays, ${name}!`
   },
   
-  happyHolidayTo: happyHolidayTo = (holiday,name) =>{
+  happyHolidayTo: function(holiday,name){
     return `Happy ${holiday}, ${name}!`
   },
   
-  holidayCountdown: holidayCountdown = (days,holiday)=>{
+  holidayCountdown: function(days,holiday){
     return `It's ${days} days until ${holiday}!`
   }
 }
