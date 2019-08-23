4ead214eebf72fe3ea8acf9235ef1de6a2281900
Hamidou Diallo <dio.hamidou@gmail.com>
2019-08-23 05:22:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
+modules.export = {
 
-
-var happyHolidays = () =>{
-  return "Happy holidays!"
+  var happyHolidays = () =>{
+    return "Happy holidays!"
+  }
+  
+  var happyHolidaysTo = name =>{
+    return `Happy holidays, ${name}!`
+  }
+  
+  var happyHolidayTo = (holiday,name) =>{
+    return `Happy ${holiday}, ${name}!`
+  }
+  
+  var holidayCountdown = (days,holiday)=>{
+    return `It's ${days} days until ${holiday}!`
+  }
 }
-
-var happyHolidaysTo = name =>{
-  return `Happy holidays, ${name}!`
-}
-
-var happyHolidayTo = (holiday,name) =>{
-  return `Happy ${holiday}, ${name}!`
-}
-
-var holidayCountdown = (days,holiday)=>{
-  return `It's ${days} days until ${holiday}!`
-}
-
 // Testing
 console.log(happyHolidays())
 console.log(happyHolidaysTo("Hamidou"))
 console.log(happyHolidayTo("thanksgiving","Anne"))
 console.log(holidayCountdown(3,"christmas"))
 
-modules.export = {happyHolidays,happyHolidaysTo,happyHolidayTo,holidayCountdown}
+
 
